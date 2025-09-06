3a158ef0275d907e07c0b49c0a591831e558a59b
Sandhani Shaik <sandhanishaik@lexonit.com>
2025-09-06 12:52:16 +0530

    agent tool migration

--- src/inngest/utills.ts
+++ src/inngest/utills.ts
@@ -1,6 +1,21 @@
-import {Sandbox} from "@e2b/code-interpreter";
+import { Sandbox } from "@e2b/code-interpreter";
+import { AgentResult, TextMessage } from "@inngest/agent-kit";
 
-export async function getSandboxUrl(sandboxId: string){
+export async function getSandboxUrl(sandboxId: string) {
   const sandbox = await Sandbox.connect(sandboxId);
   return sandbox;
+}
+
+export function lastAssistanttextMessageContent(result: AgentResult) {
+  const lastAssistanttextMessageIndex = result.output.findLastIndex(
+    (message) => message.role === "assistant"
+  );
+  const message = result.output[lastAssistanttextMessageIndex] as
+    | TextMessage
+    | undefined;
+  return message?.content
+    ? typeof message.content === "string"
+      ? message.content
+      : message.content.map((m) => m.text).join("")
+    : undefined;
 }
